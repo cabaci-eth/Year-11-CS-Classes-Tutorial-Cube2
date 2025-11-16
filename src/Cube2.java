aaa4a7889bad4caf4f6e4d64f298dcd46453c115
Miguel Wang <cabaci@protonmail.com>
2025-11-16 17:59:24 +0800

    finished

--- src/Cube2.java
+++ src/Cube2.java
@@ -12,18 +12,78 @@ public class Cube2 {
         basicCube.setSide(side);
     }
 
+    public Cube2(int side, String color) {
+        basicCube = new Cube(side);
+        this.color = color;
+    }
+
     public int getSide() {
         return basicCube.getSide();
     }
 
+    public void setSide(int side){
+        basicCube.setSide(side);
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
     //the following methods have been started for you, but need to be completed
-    public int calculateVolume() { return 0;}
-    public int calculateSurfaceArea() { return 0;}
-    public Cube2 add(Cube2 otherCube) { return new Cube2(); }
-    public Cube2 minus(Cube2 otherCube) { return new Cube2(); }
-    public boolean equals(Cube2 otherCube) { return false; }
+    public int calculateVolume(){
+        return basicCube.getSide()*basicCube.getSide()*basicCube.getSide();
+    }
+
+    public int calculateSurfaceArea(){
+        return basicCube.getSide()*basicCube.getSide()*6;
+    }
+
+    public Cube2 add(Cube2 otherCube){
+        int a = this.getSide();
+        int b = otherCube.getSide();
+
+        int cSquared = a*a + b*b;
+        int c = (int)Math.sqrt(cSquared);
+
+        if(c*c != cSquared) {
+            throw new IllegalArgumentException("Side lengths do not form a Pythagorean triple.");
+        }
+
+        return new Cube2(c, this.color);
+    }
+
+    public Cube2 minus(Cube2 otherCube){
+        int a = this.getSide();
+        int b = otherCube.getSide();
+
+        int saDiff = Math.abs(6*a*a - 6*b*b);
+        if(saDiff%6 != 0){
+            throw new IllegalArgumentException("Surface areas do not form a valid cube.");
+        }
+
+        int cSquared = saDiff/6;
+        int c = (int)Math.sqrt(cSquared);
+
+        if(c * c != cSquared){
+            throw new IllegalArgumentException("Resulting cube side is not an integer.");
+        }
+
+        if(!(a*a == b*b + cSquared || b*b == a*a + cSquared)){
+            throw new IllegalArgumentException("Side lengths do not form a Pythagorean triple.");
+        }
+
+        return new Cube2(c, this.color);
+    }
+
+    public boolean equals(Cube2 otherCube){
+        return basicCube.getSide()==otherCube.getSide() && color.equals(otherCube.getColor());
+    }
 
     public String toString() {
-        return basicCube.toString();
+        return "Cube{side="+String.valueOf(basicCube.getSide())+", color=\""+color+"\"}";
     }
 }
